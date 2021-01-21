9d4295bce241f17d667870d82b3519120a3a4a24
mdimran1409036 <mdimran1409036@gmail.com>
2021-01-21 12:53:47 +0600

    sign up and login added && switching between them were made

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form action="" @submit.prevent="handleSubmit">
+    <input type="text" placeholder="display name" v-model="displayName" required >
+    <input type="email" placeholder="email" v-model="email" required >
+    <input type="password" placeholder="password" v-model="password" required >
+    <button>Sign Up</button>
+  
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+
+    setup(){
+        const displayName = ref('')
+        const email = ref('')
+        const password = ref('')
+
+        const handleSubmit= () => {
+            console.log(displayName.value , email.value, password.value)          
+        }
+        
+        return { displayName, email, password, handleSubmit }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,11 +1,32 @@
 <template>
     <div class="welcome container">
         <p>Welcome to the chat room</p>
+        <div v-if="showLogin">
+            <h2>Login Form</h2>
+            <LoginForm/>
+            <p>Not registerd? <span @click="showLogin = !showLogin">Sign Up</span>  here</p>
+        </div>
+        <div v-else>
+            <h2> Sign Up Form</h2>
+            <SignupForm />
+            <p>Already registerd? <span @click="showLogin = !showLogin">Login</span>  here</p>
+        </div>    
+        
     </div>
 </template>
 
 <script>
+import SignupForm from '@/components/SignupForm'
+import LoginForm from '@/components/LoginForm'
+import { ref } from 'vue'
 export default {
+    components: { SignupForm, LoginForm },
+
+    setup(){
+        const showLogin = ref(true)
+    
+        return { showLogin }
+    }
 
 }
 </script>
@@ -15,6 +36,39 @@ export default {
 .welcome{
     padding:20px 0;
     text-align:center;
+}
+
+.welcome form{
+    width:300px;
+    margin:20px auto;
+}
+
+.welcome label{
+
+    display: block;
+    margin:20px 0 10px;
+
+}
+
+.welcome input{
+    width:100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;    
+}
+
+.welcome span{
+    font-weight: bold;
+    text-decoration: underline;
+    cursor: pointer;
+
+}
+
+.welcome button{
+    margin: 20px auto
 }
 
 </style>
