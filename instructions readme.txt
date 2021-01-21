5ca3229a876f59988a72d84d5cee260da3f2f16b
mdimran1409036 <mdimran1409036@gmail.com>
2021-01-21 12:07:47 +0600

    firebase setup done

---
+++ instructions readme.txt
@@ -0,0 +1,5 @@
+*** set up firebase
+
+	1.create an app with new or existing project
+	2.copy the config code lines into project directory 'firebase/config.js
+	3.install firebase : npm install firebse 
---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpfF-V2HIxXKxTo_h4LMFDL01ChdWl5uQ",
+  authDomain: "net-ninja-vue-project.firebaseapp.com",
+  databaseURL: "https://net-ninja-vue-project-default-rtdb.firebaseio.com",
+  projectId: "net-ninja-vue-project",
+  storageBucket: "net-ninja-vue-project.appspot.com",
+  messagingSenderId: "54603329231",
+  appId: "1:54603329231:web:dbae388077bbf0ff16d1ba",
+  measurementId: "G-D9ZXNT5VRC"
+}; 
+
+
+//init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export {projectFirestore, timestamp}
