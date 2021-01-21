834be4fe90bf7514258bd05bff48e82cff51d8b8
mdimran1409036 <mdimran1409036@gmail.com>
2021-01-21 11:45:34 +0600

    welcome page added as well as the stylings

---
+++ src/views/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="welcome container">
+        <p>Welcome to the chat room</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.welcome{
+    padding:20px 0;
+    text-align:center;
+}
+
+</style>
