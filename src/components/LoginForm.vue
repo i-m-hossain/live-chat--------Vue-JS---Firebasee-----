87c53aa847cc18441d187513c77fa7caa86be401
mdimran1409036 <mdimran1409036@gmail.com>
2021-01-21 19:06:23 +0600

    useLogin.js composable is added and error displayed for the login users

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,6 +2,8 @@
   <form action="" @submit.prevent="handleSubmit">
     <input type="email" placeholder="email" v-model="email" required >
     <input type="password" placeholder="password" v-model="password" required >
+
+    <div class="error">{{error}}</div>
     <button>Login</button>
   
   </form>
@@ -9,17 +11,26 @@
 
 <script>
 import { ref } from 'vue'
+import useLogin from '@/composables/useLogin'
+
 export default {
 
     setup(){
-        const email = ref('')
+
+        const {error, login} = useLogin() //composable function 'useLogin'
+        //ref
+        const email = ref('') 
         const password = ref('')
 
-        const handleSubmit= () => {
-            console.log( email.value, password.value)          
+        const handleSubmit= async() => {
+            await login(email.value, password.value)
+            if(!error.value){
+                console.log('user login success')   
+            }
+                   
         }
         
-        return { email, password, handleSubmit }
+        return { email, password, handleSubmit,error }
     }
 
 }
