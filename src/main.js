a1701d93664ba84d02d145f1b07cff4cbe9a1f4c
mdimran1409036 <mdimran1409036@gmail.com>
2021-01-22 10:05:01 +0600

    waiting for firebase auth to initialise

--- src/main.js
+++ src/main.js
@@ -3,4 +3,22 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+
+
+//During authentication, when the window is refreshed the user become null
+// To solve this we have to hold untill firebase establishes initial connection. 
+//After the browser refreshed user becomes null, but after sometime firebase establish connection 
+//that is when stateChange occurs and the project will load only after the stateChange   
+
+//import firebase auth serrvice 
+import { projectAuth } from './firebase/config'
+
+let app
+
+projectAuth.onAuthStateChanged(() => {
+    if (!app) {
+       app = createApp(App).use(router).mount('#app') 
+    }
+})
+
+
